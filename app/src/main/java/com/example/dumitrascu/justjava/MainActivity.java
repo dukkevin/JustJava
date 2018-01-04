7098cdaaadf33ee2809ab233f9050e1838703b25
dukkevin <dukkevin@gmail.com>
2018-01-04 15:39:42 +0200

    Lesson 7: Making an App Interactive: Part 2

--- app/src/main/java/com/example/dumitrascu/justjava/MainActivity.java
+++ app/src/main/java/com/example/dumitrascu/justjava/MainActivity.java
@@ -49,7 +49,9 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        String priceMessage = "Total: $" + (quantity*5) + '\n' + "Thank you!";
+        int price = quantity*5;
+        String priceMessage = "Total: $" + price;
+        priceMessage = priceMessage + "\n Thank you!";
         displayMessage (priceMessage);
     }
 
