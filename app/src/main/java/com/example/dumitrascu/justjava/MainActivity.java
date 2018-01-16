5d5d5ad2371406c5f36e2c4351b114ec07d82ac0
dukkevin <dukkevin@gmail.com>
2018-01-16 23:29:34 +0200

    Lesson 11

--- app/src/main/java/com/example/dumitrascu/justjava/MainActivity.java
+++ app/src/main/java/com/example/dumitrascu/justjava/MainActivity.java
@@ -98,7 +98,7 @@ public class MainActivity extends AppCompatActivity {
      * This method displays the given price on the screen.
      */
     private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        TextView priceTextView = (TextView) findViewById(R.id.quantity_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 
